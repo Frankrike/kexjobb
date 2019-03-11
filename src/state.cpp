accf5c8188537608caa616feb5877c14b6aa3338
Fredrik Hernqvist <hernqv@kth.se>
2019-03-11 22:28:29 +0100

    fix bug in state

--- src/state.cpp
+++ src/state.cpp
@@ -5,7 +5,7 @@ namespace state {
 
   State::State(mission::Mission mission) {
     robots.resize(mission.robots.size());
-    stations.resize(mission.robots.size());
+    stations.resize(mission.stations.size());
     for(int i = 0; i < int(robots.size()); i++) {
         robots[i].pos = mission.robots[i].startPos; // Currently at start position
         robots[i].item = -1; // Holding no item
