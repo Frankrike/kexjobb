909bbcd0fba2520948e77df2d19eb27ff309198d
Fredrik Hernqvist <hernqv@kth.se>
2019-03-23 04:41:52 +0100

    finish gui

--- src/gui.cpp
+++ src/gui.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include <math.h>
+#include <sstream>
 
 using namespace std;
 
@@ -22,10 +23,18 @@ const int LHEIGHT = 25, LMARGIN = 2;
 const int RWIDTH = 150;
 sf::Color BACKGROUNDCOL = sf::Color::Black,
   FLOORCOL = sf::Color::White,
-  ROBOTCOL = sf::Color::Red;
+  ROBOTCOL = sf::Color(0xff3333ff),
+  ITEMCOL =  sf::Color(0x3333ffff),
+  ITEMEMPTYCOL = sf::Color(0x333333ff),
+  ITEMFRAMECOL = sf::Color::Black,
+  STATIONCOL = sf::Color(0x337733ff);
+const float ITEMRADROBOT = 0.15, ITEMRADSHELF = 0.2, ITEMRADSTATION = 0.1;
 
 sf::Vector2f onMap(int x, int y) {
   return mTopRight + sf::Vector2f(x*sw, y*sw);
+}
+sf::Vector2f onMap(pair<int, int> p) {
+  return onMap(p.first, p.second);
 }
 sf::Vector2f onMapC(int x, int y) {
   return mTopRight + sf::Vector2f(x*sw + sw/2, y*sw + sw/2);
@@ -42,6 +51,10 @@ sf::Vector2f interp(sf::Vector2f a, sf::Vector2f b, float f) {
   f = smooth(f);
   return a*(1-f) + b*f;
 }
+float interp(float a, float b, float f){
+  f = smooth(f);
+  return a*(1-f) + b*f;
+}
 
 void GUI::show() {
   if (states.size() == 0)
@@ -55,7 +68,7 @@ void GUI::show() {
   int stateId = 0;
   float trans = 0;
 
-  bool playing = false;
+  bool playing = true;
 
   while(window.isOpen()) {
     bool mouseClicked = false;
@@ -112,6 +125,27 @@ void GUI::show() {
       window.draw(rectangle);
     }
 
+    auto drawText = [&](sf::Vector2f pos, sf::Color color, string str, int sz) {
+      sf::Text text(str, font, sz);
+      text.setColor(color);
+      text.setPosition(pos);
+      window.draw(text);
+    };
+    auto drawItem = [&](sf::Vector2f pos, float rad, int item, bool empty=false) {
+      sf::CircleShape circle;
+      circle.setFillColor(empty ? ITEMEMPTYCOL : ITEMCOL);
+      circle.setOutlineColor(ITEMFRAMECOL);
+      circle.setRadius(rad);
+      circle.setOutlineThickness(rad*0.1);
+      circle.setPosition(pos - sf::Vector2f(rad, rad));
+      window.draw(circle);
+      
+      drawText(pos - sf::Vector2f(rad*0.2, rad*0.5),
+        ITEMFRAMECOL,
+        to_string(item),
+        int(rad));
+    };
+
     {
       int curWidth = LHEIGHT/2;
       auto drawButton = [&](int w, string label) {
@@ -120,12 +154,10 @@ void GUI::show() {
         sf::Vector2i mp = sf::Mouse::getPosition(window);
         mouseOver = (mp.x >= x && mp.y >= y && mp.x <= x+w && mp.y <= y+h);
 
-        sf::Text text(label, font, (LHEIGHT-LMARGIN*2));
-        text.setPosition(x, y);
-        text.setColor(sf::Color(0xccccccff));
-        if (mouseOver)
-          text.setColor(sf::Color(0xffffffff));
-        window.draw(text);
+        drawText(sf::Vector2f(x, y), 
+            mouseOver ? sf::Color(0xccccccff) : sf::Color(0xffffffff),
+            label,
+            (LHEIGHT-LMARGIN*2));
         curWidth += w;
 
         return mouseOver && mouseClicked;
@@ -168,18 +200,84 @@ void GUI::show() {
         window.draw(rectangle);
       } 
 
+    {
+      int curHeight = 5, lineHeight = LHEIGHT/2, x = width-RWIDTH+LMARGIN;
+
+      {
+        drawText(sf::Vector2f(x, curHeight), sf::Color::White, "Orders", lineHeight*2);
+        curHeight += lineHeight*3;
+      }
+
+      for (int i = 0; i < int(mission.orders.size()); i ++) {
+        mission::order order = mission.orders[i];
+        stringstream ss;
+        for (int j = 0; j < int(curS.stations.size()); j++)
+          if (curS.stations[j].order == i) {
+            ss << j << "> ";
+          }
+        ss << "[";
+        for (int j = 0; j < int(order.items.size()); j++)
+          ss << (j == 0 ? "" : ", ") << order.items[j];
+        ss << "]";
+        drawText(sf::Vector2f(x, curHeight), sf::Color::White, ss.str(), lineHeight);
+        curHeight += lineHeight;
+      }
+    }
+
+    for (int i = 0; i < int(curS.stations.size()); i++) {
+      sf::RectangleShape rectangle;
+      rectangle.setFillColor(STATIONCOL);
+      rectangle.setPosition(onMap(mission.stations[i].coors));
+      rectangle.setSize(sf::Vector2f(sw, sw));
+      window.draw(rectangle);
+
+      state::station curSt = curS.stations[i], nextSt = nextS.stations[i];
+      sf::Vector2f pos = onMapC(mission.stations[i].coors);
+      drawText(pos + sf::Vector2f(-sw*0.37, sw*0.15), sf::Color::Black, "station " + to_string(i), int(sw*0.2));
+      if (curSt.order == -1)
+        continue;
+      mission::order order = mission.orders[curSt.order];
+      for (int j = 0; j < int(order.items.size()); j++) {
+        double ang = 2*M_PI*j/order.items.size();
+        sf::Vector2f itemPos = pos + sf::Vector2f(float(cos(ang)), float(sin(ang)))*sw*0.3f;
+        if (order.items.size() == 1)
+          itemPos = pos;
+        drawItem(itemPos, ITEMRADSTATION*sw, order.items[j], !curSt.fulfilled[j]);
+        if (!curSt.fulfilled[j] && (curSt.order != nextSt.order || nextSt.fulfilled[j]))
+          drawItem(itemPos, interp(0, ITEMRADSTATION, trans)*sw, order.items[j]);
+      }
+    }
+
     for (int r = 0; r < int(curS.robots.size()); r++) {
+      state::robot curR = curS.robots[r], nextR = nextS.robots[r];
       float rad = sw*0.4f;
       sf::Vector2f pos = interp(
-        onMapC(mission.toCoors(curS.robots[r].pos)), 
-        onMapC(mission.toCoors(nextS.robots[r].pos)), trans)
-          - sf::Vector2f(rad, rad);
+        onMapC(mission.toCoors(curR.pos)), 
+        onMapC(mission.toCoors(nextR.pos)), trans);
 
       sf::CircleShape circle;
       circle.setFillColor(ROBOTCOL);
       circle.setRadius(rad);
-      circle.setPosition(pos);
+      circle.setPosition(pos - sf::Vector2f(rad, rad));
       window.draw(circle);
+
+      if (curR.item == -1 && nextR.item != -1) {
+        float itemR = sw*interp(ITEMRADSHELF, ITEMRADROBOT, trans);
+        sf::Vector2f from = onMapC(mission.items[nextR.item].shelfCoors);
+        sf::Vector2f itemPos = interp(from, pos, trans);
+        drawItem(itemPos, itemR, nextR.item);
+      } else if (curR.item != -1 && nextR.item == -1) {
+        drawItem(pos, sw*interp(ITEMRADROBOT, 0, trans), curR.item);
+      } else if (curR.item != -1){
+        drawItem(pos, sw*ITEMRADROBOT, curR.item);
+      }
+    }
+
+    for (int i = 0; i < int(mission.items.size()); i ++) {
+      mission::item item = mission.items[i];
+      sf::Vector2f pos = onMapC(item.shelfCoors);
+      drawItem(pos, sw*ITEMRADSHELF, i);
+      drawText(pos += sf::Vector2f(-sw*0.3, sw*0.15), sf::Color(0x444444ff), "shelf", int(sw*0.2));
     }
 
     window.display();
