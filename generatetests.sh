5044e4add28c6d633a33f0063af5aa89a670a968
Fredrik Hernqvist <fredrikhernqvist96@gmail.com>
2019-04-02 13:56:37 +0200

    add generate shelf tests

--- generatetests.sh
+++ generatetests.sh
@@ -1,3 +1,4 @@
 make
-./missiongen.out seed=0 w=10 h=10 mode=random density=0.2 robots=2 stations=2 orders=3 ordersize=3 items=2 > missions/gen1
-./missiongen.out seed=0 w=20 h=20 mode=random density=0.05 robots=5 stations=5 orders=30 ordersize=5 items=10 > missions/gen2
+./missiongen.out seed=0 w=10 h=10 mode=random density=0.2 robots=2 stations=2 orders=3 ordersize=3 items=2 > missions/rand0.txt
+./missiongen.out seed=0 w=20 h=20 mode=random density=0.05 robots=5 stations=5 orders=30 ordersize=5 items=10 > missions/rand1.txt
+./missiongen.out seed=0 mode=shelves shelvesX=3 shelvesY=2 margin=2 marginStations=3 robots=5 stations=5 orders=30 ordersize=5 > missions/shelves0.txt
